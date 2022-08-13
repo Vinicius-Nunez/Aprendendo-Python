8d0c2ba92c9699f21d13dade0b1dc4c8ca4e0afd
Vinicius Henrique Nunez Lima <viniciusnunez69@gmail.com>
2022-08-13 13:49:34 -0300

    aula1

---
+++ Intro/aula01.py
@@ -0,0 +1,11 @@
+#nome=input('qual o seu nome?')
+#print('Bem Vindo(a)', nome)
+
+#dia= str (input('dia de nascimento?'))
+#mes= str (input('mes de nascimento?'))
+#ano= str (input('ano de nascimento?'))
+#print('voce nasceu em',dia,'/', mes,'/', ano)
+
+num1= int(input('digite um numero:'))
+num2= int(input('digite o segundo numero:'))
+print('A soma de',num1,'+', num2,'é', num1 + num2)
