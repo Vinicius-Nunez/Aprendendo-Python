aa69f33009f9c97bbe3c1e6f86e36c30863c29af
Vinicius Henrique Nunez Lima <viniciusnunez69@gmail.com>
2022-08-14 12:22:25 -0300

    uau

---
+++ Exercicios/exer12.py
@@ -0,0 +1,3 @@
+preco=float(input('Digite o valor do produto: '))
+desc= preco*0.05
+print(f'valor do produto é RS{preco:.2f} e com um desconto de {desc} ficara RS{preco-desc:.2f}')
---
+++ Exercicios/exer13.py
@@ -0,0 +1,3 @@
+preco=float(input('Digite o valor do produto: '))
+desc= preco*0.15
+print(f'Seu salario é RS{preco:.2f} e com um aumento de {desc} ficara RS{preco+desc:.2f}')
